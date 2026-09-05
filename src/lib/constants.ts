82ae634200bb2edf22aab4a20aab5a54fdfcc7de
jihyuuk <jihyuk.dev@gmail.com>
2026-09-05 15:48:13 +0900

    refactor: 기본 차트 스케일 상수로 관리

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,3 +3,4 @@ export const API_URL = import.meta.env.VITE_API_URL;
 export const INFO_HEIGHT = 64;
 export const STICKER_SIZE = 40;
 export const DEFAULT_GAP = 8;
+export const DEFALUT_CHART_SCALE = 100; //10단위로
