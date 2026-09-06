2fb3f52b554726c7fa4a4ba440f9d40ffe2ac5a6
jihyuuk <jihyuk.dev@gmail.com>
2026-09-07 01:28:49 +0900

    feat: 타이틀 이미지 레이아웃 깨짐 방지

--- src/components/header-image.tsx
+++ src/components/header-image.tsx
@@ -1,5 +1,13 @@
 import titleImage from "@/assets/title.webp";
 
 export default function HeaderImage() {
-  return <img src={titleImage} className="mx-auto w-full max-w-200 pt-5" />;
+  return (
+    <img
+      src={titleImage}
+      width={1600} //미리 이미지 크기 알려줘서 레이아웃 틀어짐 방지
+      height={317}
+      fetchPriority="high" //최대한 먼저 로딩
+      className="mx-auto w-full max-w-200 pt-5"
+    />
+  );
 }
