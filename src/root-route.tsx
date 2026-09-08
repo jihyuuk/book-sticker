939712b7502eadd352958d6a7d23508625931a32
jihyuuk <jihyuk.dev@gmail.com>
2026-09-08 19:38:51 +0900

    feat: 이전 공개 차트 경로 수정 및 post 관련 라우트 설정

--- src/root-route.tsx
+++ src/root-route.tsx
@@ -3,35 +3,41 @@ import AdminPage from "./pages/admin-page";
 import ChartPage from "./pages/chart-page";
 import GuestOnlyRoute from "./routes/guest-only-route";
 import UserOnlyRoute from "./routes/user-only-route";
-import SignInPage from "./pages/sign-in-page";
-import SignUpPage from "./pages/sign-up-page";
+import SignInPage from "./pages/auth/sign-in-page";
+import SignUpPage from "./pages/auth/sign-up-page";
 import PublicChartPage from "./pages/public-chart-page";
-import VerifyEmailPage from "./pages/verify-email-page";
-import PostCreatePage from "./pages/post-create-page";
-import PublicClassroomRoute from "./routes/public-classroom-route";
-import PostDetailPage from "./pages/post-detail-page";
+import VerifyEmailPage from "./pages/auth/verify-email-page";
+import PostCreatePage from "./pages/post/post-create-page";
+import PostDetailPage from "./pages/post/post-detail-page";
+import PostListPage from "./pages/post/post-list-page";
 
 export default function RootRoute() {
   return (
     <Routes>
+      {/* 게스트 (로그인 X)*/}
       <Route element={<GuestOnlyRoute />}>
         <Route path="/sign-in" element={<SignInPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
         <Route path="/verify-email" element={<VerifyEmailPage />} />
       </Route>
 
+      {/* 유저 (로그인 O) */}
       <Route element={<UserOnlyRoute />}>
         <Route path="/" element={<ChartPage />} />
         <Route path="/admin" element={<AdminPage />} />
       </Route>
 
-      {/* 공유 공개 페이지 */}
-      <Route path="/share/:publicId" element={<PublicChartPage />} />
+      {/* 퍼블릭(공유 링크) 아무나 접근 */}
+      <Route path="/classroom/:publicId">
+        <Route index element={<Navigate to="chart" replace />} />
 
-      {/* 임시 개발용 포스트 생성 페이지 */}
-      <Route path="/classroom/:publicId" element={<PublicClassroomRoute />}>
-        <Route path="posts/new" element={<PostCreatePage />} />
-        <Route path="posts/:postId" element={<PostDetailPage />} />
+        <Route path="chart" element={<PublicChartPage />} />
+
+        <Route path="posts">
+          <Route index element={<PostListPage />} />
+          <Route path="new" element={<PostCreatePage />} />
+          <Route path=":postId" element={<PostDetailPage />} />
+        </Route>
       </Route>
 
       {/* 잘 못 된 경로 */}
