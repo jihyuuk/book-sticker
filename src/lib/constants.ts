045cc051daaab990ed05a17f96fc2ee86ceb7017
jihyuuk <jihyuk.dev@gmail.com>
2026-09-09 23:42:10 +0900

    modify: DEFALUT_CHART_SCALE -> 30 으로 수정

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,4 +4,4 @@ export const DOMAIN_URL = import.meta.env.VITE_DOMAIN_URL;
 export const INFO_HEIGHT = 64;
 export const STICKER_SIZE = 40;
 export const DEFAULT_GAP = 8;
-export const DEFALUT_CHART_SCALE = 100; //10단위로
+export const DEFALUT_CHART_SCALE = 30; //10단위로
